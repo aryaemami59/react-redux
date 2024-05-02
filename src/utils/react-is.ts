184ee775f7e6609943515b829f632e2f1a8ccb97
Arya Emami <aryaemami59@yahoo.com>
2024-05-02 01:20:35 -0500

    Fix remaining issues in `react-is.ts`

--- src/utils/react-is.ts
+++ src/utils/react-is.ts
@@ -6,7 +6,7 @@ import React from 'react'
 // It's very possible this could change in the future, but given that
 // we only use these in `connect`, this is a low priority.
 
-const IS_REACT_19 = React.version.startsWith('18')
+const IS_REACT_19 = React.version.startsWith('19')
 
 const REACT_ELEMENT_TYPE = Symbol.for(
   IS_REACT_19 ? 'react.transitional.element' : 'react.element',
@@ -26,31 +26,35 @@ const REACT_LAZY_TYPE = Symbol.for('react.lazy')
 const REACT_OFFSCREEN_TYPE = Symbol.for('react.offscreen')
 const REACT_CLIENT_REFERENCE = Symbol.for('react.client.reference')
 
+export const ForwardRef = REACT_FORWARD_REF_TYPE
+export const Memo = REACT_MEMO_TYPE
+
 export function isValidElementType(type: any): type is ElementType {
-  return 'string' === typeof type ||
-    'function' === typeof type ||
+  return typeof type === 'string' ||
+    typeof type === 'function' ||
     type === REACT_FRAGMENT_TYPE ||
     type === REACT_PROFILER_TYPE ||
     type === REACT_STRICT_MODE_TYPE ||
     type === REACT_SUSPENSE_TYPE ||
     type === REACT_SUSPENSE_LIST_TYPE ||
     type === REACT_OFFSCREEN_TYPE ||
-    ('object' === typeof type &&
-      null !== type &&
+    (typeof type === 'object' &&
+      type !== null &&
       (type.$$typeof === REACT_LAZY_TYPE ||
         type.$$typeof === REACT_MEMO_TYPE ||
         type.$$typeof === REACT_CONTEXT_TYPE ||
         type.$$typeof === REACT_CONSUMER_TYPE ||
         type.$$typeof === REACT_FORWARD_REF_TYPE ||
         type.$$typeof === REACT_CLIENT_REFERENCE ||
-        undefined !== type.getModuleId))
+        type.getModuleId !== undefined))
     ? !0
     : !1
 }
 
-export function typeOf(object: any) {
-  if ('object' === typeof object && null !== object) {
-    const $$typeof = object.$$typeof
+function typeOf(object: any): symbol | undefined {
+  if (typeof object === 'object' && object !== null) {
+    const { $$typeof } = object
+
     switch ($$typeof) {
       case REACT_ELEMENT_TYPE:
         switch (((object = object.type), object)) {
@@ -79,58 +83,12 @@ export function typeOf(object: any) {
   }
 }
 
-export const ContextConsumer = REACT_CONSUMER_TYPE
-export const ContextProvider = REACT_CONTEXT_TYPE
-export const Element = REACT_ELEMENT_TYPE
-export const ForwardRef = REACT_FORWARD_REF_TYPE
-export const Fragment = REACT_FRAGMENT_TYPE
-export const Lazy = REACT_LAZY_TYPE
-export const Memo = REACT_MEMO_TYPE
-export const Portal = REACT_PORTAL_TYPE
-export const Profiler = REACT_PROFILER_TYPE
-export const StrictMode = REACT_STRICT_MODE_TYPE
-export const Suspense = REACT_SUSPENSE_TYPE
-export const SuspenseList = REACT_SUSPENSE_LIST_TYPE
-
 export function isContextConsumer(object: any): object is ReactElement {
   return IS_REACT_19
     ? typeOf(object) === REACT_CONSUMER_TYPE
     : typeOf(object) === REACT_CONTEXT_TYPE
 }
-export function isContextProvider(object: any) {
-  return typeOf(object) === REACT_CONTEXT_TYPE
-}
-export function isElement(object: any) {
-  return (
-    typeof object === 'object' &&
-    object !== null &&
-    object.$$typeof === REACT_ELEMENT_TYPE
-  )
-}
-export function isForwardRef(object: any) {
-  return typeOf(object) === REACT_FORWARD_REF_TYPE
-}
-export function isFragment(object: any) {
-  return typeOf(object) === REACT_FRAGMENT_TYPE
-}
-export function isLazy(object: any) {
-  return typeOf(object) === REACT_LAZY_TYPE
-}
+
 export function isMemo(object: any): object is MemoExoticComponent<any> {
   return typeOf(object) === REACT_MEMO_TYPE
-}
-export function isPortal(object: any) {
-  return typeOf(object) === REACT_PORTAL_TYPE
-}
-export function isProfiler(object: any) {
-  return typeOf(object) === REACT_PROFILER_TYPE
-}
-export function isStrictMode(object: any) {
-  return typeOf(object) === REACT_STRICT_MODE_TYPE
-}
-export function isSuspense(object: any) {
-  return typeOf(object) === REACT_SUSPENSE_TYPE
-}
-export function isSuspenseList(object: any) {
-  return typeOf(object) === REACT_SUSPENSE_LIST_TYPE
 }
